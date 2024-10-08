294d6c10175ffd52b05744bc844b1b4d0e749d3f
joacosanchez12 <joaquin.sancheezz@gmail.com>
2024-10-08 03:03:16 +0000

    Simple Counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,72 @@
 import React from "react";
+import SecondsCounter from "./secondsCounter";
 
-//include images into your bundle
+//import img
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    let seconds = 0; 
+    let intervalId = null; 
+    let isPaused = false; 
+
+    const startTimer = () => {
+        if (!intervalId && !isPaused) {
+            intervalId = setInterval(() => {
+                seconds += 1;
+                updateDisplay(); 
+            }, 1000);
+        }
+    };
+
+    const stopTimer = () => {
+        clearInterval(intervalId);
+        intervalId = null;
+        isPaused = true; 
+    };
+
+    const resumeTimer = () => {
+        if (isPaused) {
+            startTimer(); 
+            isPaused = false; 
+        }
+    };
+
+    const resetTimer = () => {
+        clearInterval(intervalId);
+        intervalId = null;
+        seconds = 0;
+        isPaused = false; 
+        updateDisplay(); 
+    };
+
+    const updateDisplay = () => {
+        const container = document.getElementById("timer-container");
+        if (container) {
+            container.innerHTML = `
+                <p>Segundos: ${seconds}</p>
+                <button id="reset-button" class="reset-button">Reset Timer</button>
+                <button id="stop-button" class="stop-button">Stop Timer</button>
+                <button id="resume-button" class="resume-button">Resume Timer</button>
+            `;
+           
+            document.getElementById("reset-button").onclick = resetTimer;
+            document.getElementById("stop-button").onclick = stopTimer;
+            document.getElementById("resume-button").onclick = resumeTimer;
+        }
+    };
+
+    
+    startTimer();
+
+    
+    updateDisplay();
+
+    return (
+        <div id="timer-container">
+            {/*  */}
+        </div>
+    );
 };
 
 export default Home;
